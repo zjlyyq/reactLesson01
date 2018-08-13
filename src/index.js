3303f8b2566621579758d797b2b1f411ce39fe03
zjlyyq <2868989685@qq.com>
2018-08-13 11:30:32 +0800

    'sys'

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import './index.css';
 
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+    return ( <button className = "square"
+        onClick = { props.onClick } > { props.value } </button>
+    );
 }
 
 // class Square extends React.Component {
@@ -27,112 +25,155 @@ function Square(props) {
 // }
 
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      values:Array(9).fill(null),
-      isNextX:false,
+    // constructor(props){
+    //   super(props);
+    //   this.state = {
+    //     values:this.props.values,
+    //     isNextX:false,
+    //   }
+    // }
+    renderSquare(i) {
+        return <Square
+            value = { this.props.values[i]}
+            onClick = {
+            () => this.props.onClick(i)
+            }
+            />;
     }
-  }
-  // 处理点击时间
-  handleClick(i){
-      const squares = this.state.values.slice();
-      squares[i] = this.state.isNextX?'X':'O';
-      if(tellMeWhoWin(this.state.values)){
-        return;
-      }
-      this.setState({
-        values: squares,
-        isNextX:!this.state.isNextX,
-      });
-  }
-  renderSquare(i) {
-    return <Square 
-    value={this.state.values[i]}
-    onClick = {()=>this.handleClick(i)}
-    />;
-  }
 
-  render() {
-    //const是什么意思，而且为什么函数放在这里，每次都能够执行，不是应该只能执行一次吗
-    const winner = tellMeWhoWin(this.state.values);  
-
-    let status;   //这个let是什么意思
-    if (winner) {
-        status = 'winner is  ' + winner;
+    render() {
+        return ( <
+            div >
+            <
+            div className = "board-row" > { this.renderSquare(0)} { this.renderSquare(1)} { this.renderSquare(2)} <
+            /div> <
+            div className = "board-row" > { this.renderSquare(3)} { this.renderSquare(4)} { this.renderSquare(5)} <
+            /div> <
+            div className = "board-row" > { this.renderSquare(6)} { this.renderSquare(7)} { this.renderSquare(8)} <
+            /div> < /
+            div >
+        );
     }
-    else{
-        status = 'Next player: ' + (this.state.isNextX?'X':'O');
-    }
-    
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
 }
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                values: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+    // 处理点击时间
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.values.slice();
+        if (tellMeWhoWin(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                values: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    // 跳转到指定步骤
+    jumpTo(i) {
+        var his = this.state.history;
+        var cur = his[i];
+        var squ = cur.values.slice();
+        var flag = ((his.length - i) % 2 == 0);
+        this.setState({
+            history: his.concat([{
+                values: squ,
+            }]),
+            xIsNext: flag ? !this.state.xIsNext : this.state.xIsNext,
+        });
+    }
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = tellMeWhoWin(current.values);
+        // 这个map函数可以说很高级了
+        const moves = history.map(
+            (step, move) => {
+                const desc = move ?
+                    'Go to move #' + move :
+                    'Go to game start';
+                return ( <
+                    li >
+                    <
+                    button onClick = {
+                    () => this.jumpTo(move)
+                    } > { desc } < /button> < /
+                    li >
+                );
+            }
+        );
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return ( <
+            div className = "game" >
+            <
+            div className = "game-board" >
+            <
+            Board values = { current.values } onClick = {
+            (i) => this.handleClick(i)
+            }
+            /> < /
+            div > <
+            div className = "game-info" >
+            <
+            div > { status } < /div> <
+            ol > { moves } < /ol> < /
+            div > <
+            /div>
+        );
+    }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+ReactDOM.render(<
+Game / >,
+    document.getElementById('root')
 );
+
 // 计算输赢结果
-  function tellMeWhoWin(values){
+function tellMeWhoWin(values) {
     const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
-    for(var i = 0;i < lines.length;i ++){
-      var j,q,k;
-      j = lines[i][0];
-      q = lines[i][1];
-      k = lines[i][2];
-      if (values[j]) {
-        if (values[j] === values[q] && values[q] === values[k]) {
-           return values[j];
+    for (var i = 0; i < lines.length; i++) {
+        var j,
+            q,
+            k;
+        j = lines[i][0];
+        q = lines[i][1];
+        k = lines[i][2];
+        if (values[j]) {
+            if (values[j] === values[q] && values[q] === values[k]) {
+                return values[j];
+            }
         }
-      }
-      
+
     }
     return null;
-  }
+}
